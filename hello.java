0876150c05ee6825772d9f3974cc56f43f56e8f4
BaeJin <qowls0527@naver.com>
2017-06-26 16:59:06 +0900

    hello.java modified GitHub

--- hello.java
+++ hello.java
@@ -1,3 +1,5 @@
+//For command line git tutorial
+//Github modification
 
 public class Hello{
 
@@ -5,9 +7,7 @@ public class Hello{
 	System.out.println("Hello world");
   	System.out.println("Hello world2");
 	System.out.println("Tell your world");
-<<<<<<< HEAD
-	=====
 	System.out.println("Tell her world");
->>>>>>> hotfix
+
   }
 }
